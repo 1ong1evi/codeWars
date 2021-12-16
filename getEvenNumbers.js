e7800b5d567af92a7c805d21c660e8ca7776cd8d
Levi Long <longlevi6@gmail.com>
2021-12-16 08:42:39 -0600

    My solution to JavaScript Array filter

---
+++ getEvenNumbers.js
@@ -0,0 +1,6 @@
+function getEvenNumbers(numbersArray){
+    // filter out the odd numbers
+    return numbersArray.filter(num => num % 2 === 0);
+  }
+
+  console.log(getEvenNumbers([1,2,3,6,8,10]));

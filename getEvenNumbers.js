7993501f99d15c6098bffd1f7d29748e1a8910ec
Levi Long <longlevi6@gmail.com>
2021-12-16 09:02:35 -0600

    Removing the test

--- getEvenNumbers.js
+++ getEvenNumbers.js
@@ -2,5 +2,3 @@ function getEvenNumbers(numbersArray){
     // filter out the odd numbers
     return numbersArray.filter(num => num % 2 === 0);
   }
-
-  console.log(getEvenNumbers([1,2,3,6,8,10]));

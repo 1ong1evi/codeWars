b2d80820e7dbc6acf993a2b0a38b9f35a7977bd4
Levi Long <longlevi6@gmail.com>
2021-12-16 08:22:54 -0600

    Adding codeWars problem Pile of Cubes

--- formatStr.js
+++ formatStr.js
@@ -1,20 +1,12 @@
 function list(names){
-     let str = ''; // return a string
-  if (names.length !== 0) { // if its not []
-       let last = names.pop();
-       str = names.map( (val, i, arr) => {
-         if (i !== arr[arr.length - 1]) {
-           return val.name;
-         }
-       }).join(', ')
-        
-       str += str !== '' ? ' & ' + last.name : last.name;
-     }
-      
-     return str;
-   
-   }
-   
+    //your code here
+ // if(names.length === 0) {
+ //     return '';
+ // } else if (names.length === 1) {
+ //     return names[0];
+ // }
+    return Object.keys(names)
+  }
   //console.log(list([{name: 'Bart'}]));
   
   //testing area

49b51f8d4652dbf174f91f47cd6db46ba31bb3c7
Levi Long <longlevi6@gmail.com>
2021-12-16 08:22:54 -0600

    Adding codeWars problem Pile of Cubes

---
+++ pileOfCubes.js
@@ -0,0 +1,9 @@
+function findNb(m) {
+    // total volume = m -> forumal pile of cubes 
+    //Bottom have n^3 above that is (n-1)^3 then the top is 1^3
+    // if no such n return - 1
+   return m = n^3 + (n-1)^3 + 1^3;
+    //return (-1);
+}
+
+findNb(4183059834009);

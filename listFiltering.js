587995c7f4f933ef336acb72e75b8bea403450bb
Levi <longlevi6@gmail.com>
2021-12-28 08:01:30 -0600

    comments made

--- listFiltering.js
+++ listFiltering.js
@@ -1,9 +1,8 @@
 function filter_list(l) {
-  // Return a new array with the strings filtered out
-  //Input strings and numbers array and filter out the strings to only have numbers
-  //filter method??? typeof === string??? can you remove a key value that is equal to a string and still have integars???
+  //holding variable i of numbers
     let i = [];
     l.forEach(function (x) {
+        //checks if the iteration is equal to number
         if (typeof x === 'number') { 
             i.push(x);
         }
